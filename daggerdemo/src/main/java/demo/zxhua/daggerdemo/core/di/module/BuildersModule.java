14201cce04b152d414291dcebb324169938cf714
zhangxianhua <zhangxianhua@gomeholdings.com>
2017-12-02 14:57:50 +0800

    BuildersModule add Scope for the method

--- daggerdemo/src/main/java/demo/zxhua/daggerdemo/core/di/module/BuildersModule.java
+++ daggerdemo/src/main/java/demo/zxhua/daggerdemo/core/di/module/BuildersModule.java
@@ -4,6 +4,8 @@ import dagger.Module;
 import dagger.android.ContributesAndroidInjector;
 import demo.zxhua.daggerdemo.core.base.BaseActivity;
 import demo.zxhua.daggerdemo.core.base.BaseFragment;
+import demo.zxhua.daggerdemo.core.di.scope.ActivityScope;
+import demo.zxhua.daggerdemo.core.di.scope.FragmentScope;
 
 /**
  * Created by Zxhua on 2017/12/2 0002.
@@ -11,9 +13,11 @@ import demo.zxhua.daggerdemo.core.base.BaseFragment;
 @Module
 public abstract class BuildersModule {
 
+    @ActivityScope
     @ContributesAndroidInjector(modules = {ActivityMoudel.class})
     abstract BaseActivity activityInjector();
 
+    @FragmentScope
     @ContributesAndroidInjector(modules = {FragmentMoudel.class})
     abstract BaseFragment fragmentInjector();
 }
